0c357a13d4d427d5c70b0319d4c941da4aabecdb
Simona Brabcová <simona.brabcova@email.cz>
2022-03-25 22:39:44 +0100

    hotový úkol

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
-// Použij výpis do konzole prohlížeče pomocí console.log('text')
-// Konzoli otevřeš v Chrome stiskem F12 (na Windows) nebo Command-Option-I (na Macu)
 
+
+// while cyklus
 
 // První příklad - vypiš vzestupně čísla od 0 do 10
 console.log('První příklad - vzestupná řada 0 - 10');
 
-//TADY NAPIS CYKLUS
+let a = 0;
 
-console.log('-------------------');
-
-
+while (a <= 10) {
+    console.log(a)
+    a++;
+}
 
 // Druhý příklad - zkopíruj předchozí příklad
 // a vyzkoušej, jaký je rozdíl mezi <= a <
@@ -17,15 +18,25 @@ console.log('-------------------');
 
 console.log('Druhý příklad - rozdíl mezi <= a <');
 
-//TADY NAPIS CYKLUS
+let b = 0;
 
-console.log('-------------------');
-
-
+while (b < 10) {
+    console.log(b)
+    b++
+}
+console.log("<=")
+let c = 0;
+while (c <= 10) {
+    console.log(c)
+    c++
+}
 
 // Třetí příklad - vypiš sestupně čísla od 10 do 0
 console.log('Třetí příklad - sestupná řada 10 - 0');
 
-//TADY NAPIS CYKLUS
+let d = 10
+while (d >= 0){
+    console.log(d)
+    d--
+}
 
-console.log('-------------------');
